6c7334ffd4c827a2047323f3fae9030fdff6bc92
(xkrrn) <(mychoco7@naver.com)>
2022-05-23 16:45:33 +0900

    교수/연구실 - 리사이클러뷰 추가

--- 1/app/src/main/java/com/example/myapplication/Lab.kt
+++ 1/app/src/main/java/com/example/myapplication/Lab.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.ui.extra.ProfileAdapter
+import com.example.myapplication.ui.extra.professor_profile
 import com.example.myapplication.ui.extra.professor_webview
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_lab.*
@@ -18,8 +21,23 @@ class Lab : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab)
 
-        professor_name.setOnClickListener {
-            startActivity(Intent(this, professor_webview::class.java)) //버튼 클릭시 웹뷰로 이동
-        }
+        val profileList = arrayListOf(
+            professor_profile("교수님1","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님2","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님3","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님4","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님5","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님6","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님7","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님8","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님9","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님10","실시간 시스템 연구실","IT1-503"),
+            professor_profile("교수님11","실시간 시스템 연구실","IT1-503"),
+       )
+
+        professor_list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        professor_list.setHasFixedSize(true)
+
+        professor_list.adapter = ProfileAdapter(profileList)
     }
 }
--- 1/app/src/main/java/com/example/myapplication/ui/extra/ProfileAdapter.kt
+++ 1/app/src/main/java/com/example/myapplication/ui/extra/ProfileAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication.ui.extra
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 
@@ -13,14 +16,18 @@ class ProfileAdapter(val profileList : ArrayList<professor_profile>) : RecyclerV
     ): ProfileAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.professor_item, parent, false)
         return CustomViewHolder(view)
+
     }
 
     override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
+        holder.name.text = profileList.get(position).name
+        holder.lab_name.text = profileList.get(position).lab_name
+        holder.room_number.text = profileList.get(position).room_number
 
     }
 
     override fun getItemCount(): Int {
-
+        return profileList.size
     }
 
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
